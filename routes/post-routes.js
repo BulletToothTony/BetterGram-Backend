f04baff336f4a1e643345491d9e6c3a1ccdd2483
Henry <henrywl@gmail.com>
2024-07-18 15:55:06 +0100

    protected routes

--- routes/post-routes.js
+++ routes/post-routes.js
@@ -7,32 +7,9 @@ const cloudinary = require("../utils/cloudinary");
 const upload = require("../middleware/multer");
 const checkAuth = require("../middleware/check-auth");
 
-
-
-
-// router.post('/upload', upload.single('image'), postsControllers.testImageUpload)
-router.post("/upload", upload.single("image"), function (req, res) {
-  cloudinary.uploader.upload(req.file.path, function (err, result) {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({
-        success: false,
-        message: "Error",
-      });
-    }
-
-    res.status(200).json({
-      success: true,
-      message: "Uploaded!",
-      data: result,
-    });
-  });
-});
-
 router.use(checkAuth);
 
-router.post("/post/:uid", upload.single('image'), postsControllers.postStatus); //move this to post routes
-
+router.post("/post/:uid", upload.single("image"), postsControllers.postStatus); //move this to post routes
 
 router.post("/likePost/:postID", postsControllers.postLikePost);
 
@@ -43,6 +20,5 @@ router.post("/commentPost/:postID", postsControllers.postComment);
 router.get("/singlePost/:postID", postsControllers.getSinglePost);
 
 router.delete("/singlePost/:postID", postsControllers.deletePost);
-
 
 module.exports = router;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -7,19 +7,24 @@ const router = express.Router()
 
 router.get('/feed/:userId', usersControllers.getFollowingPosts)
 router.get('/allusers', usersControllers.getAllUsers)
-router.post('/follow/:uid', usersControllers.postFollowUser)
-router.post('/unfollow/:uid', usersControllers.postUnfollowUser)
+const checkAuth = require("../middleware/check-auth");
+
+
+
 
 
 
 router.get('/user/:uid', usersControllers.getUserProfile)
 router.get('/singleUserFeed/:uid', usersControllers.getSingleUserFeed)
 
-router.patch('/user/updateUserProfile/:uid', upload.single('image'), usersControllers.postUserProfileUpdate)
-
 router.post('/signup', upload.single('image'), usersControllers.signup)
 router.post('/login', usersControllers.login)
 
+router.use(checkAuth);
+
+router.post('/follow/:uid', usersControllers.postFollowUser)
+router.post('/unfollow/:uid', usersControllers.postUnfollowUser)
+router.patch('/user/updateUserProfile/:uid', upload.single('image'), usersControllers.postUserProfileUpdate)
 
 
 module.exports = router;
