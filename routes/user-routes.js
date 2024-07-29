2a3a2e76265ee8314c8e525ecaa2cdb67a0e5b14
Henry <henrywl@gmail.com>
2024-07-29 13:24:26 +0100

    changes backend

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,30 +1,28 @@
-const express = require('express')
+const express = require("express");
 
-const usersControllers = require('../controllers/users-controllers')
-const upload = require('../middleware/multer')
-
-const router = express.Router()
-
-router.get('/feed/:userId', usersControllers.getFollowingPosts)
-router.get('/allusers', usersControllers.getAllUsers)
+const usersControllers = require("../controllers/users-controllers");
 const checkAuth = require("../middleware/check-auth");
+const upload = require("../middleware/multer");
+
+const router = express.Router();
 
 
+router.get("/feed/:userId", usersControllers.getFollowingPosts);
+router.get("/allusers", usersControllers.getAllUsers);
+router.get("/user/:uid", usersControllers.getUserProfile);
+router.get("/singleUserFeed/:uid", usersControllers.getSingleUserFeed);
 
-
-
-
-router.get('/user/:uid', usersControllers.getUserProfile)
-router.get('/singleUserFeed/:uid', usersControllers.getSingleUserFeed)
-
-router.post('/signup', upload.single('image'), usersControllers.signup)
-router.post('/login', usersControllers.login)
+router.post("/signup", upload.single("image"), usersControllers.signup);
+router.post("/login", usersControllers.login);
 
 router.use(checkAuth);
 
-router.post('/follow/:uid', usersControllers.postFollowUser)
-router.post('/unfollow/:uid', usersControllers.postUnfollowUser)
-router.patch('/user/updateUserProfile/:uid', upload.single('image'), usersControllers.postUserProfileUpdate)
-
+router.post("/follow/:uid", usersControllers.postFollowUser);
+router.post("/unfollow/:uid", usersControllers.postUnfollowUser);
+router.patch(
+  "/user/updateUserProfile/:uid",
+  upload.single("image"),
+  usersControllers.postUserProfileUpdate
+);
 
 module.exports = router;
